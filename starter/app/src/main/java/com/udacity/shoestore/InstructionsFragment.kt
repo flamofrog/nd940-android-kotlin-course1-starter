ea8704e9d2ee90ad9abb70738a5a72ff1e13a18c
flamofrog <flamofrog@gmail.com>
2022-03-20 14:27:58 +0000

    feat: Add nav button to instructions screen

---
+++ starter/app/src/main/java/com/udacity/shoestore/InstructionsFragment.kt
@@ -0,0 +1,21 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.databinding.FragmentInstructionsBinding
+
+class InstructionsFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        super.onCreate(savedInstanceState)
+
+        val binding: FragmentInstructionsBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_instructions, container, false)
+
+        return binding.root
+    }
+}
--- starter/app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
 
@@ -17,6 +18,9 @@ class WelcomeFragment : Fragment() {
         val binding: FragmentWelcomeBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_welcome, container, false)
 
+        binding.button.setOnClickListener {
+            findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToInstructionsFragment())
+        }
 
         return binding.root
     }
