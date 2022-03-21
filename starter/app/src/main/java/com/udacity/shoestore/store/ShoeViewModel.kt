9ba8769ede54aed5d4ca39f103d35dd847456ea6
flamofrog <flamofrog@gmail.com>
2022-03-21 23:16:41 +0000

    refactor: Unused imports

--- starter/app/src/main/java/com/udacity/shoestore/store/ShoeViewModel.kt
+++ starter/app/src/main/java/com/udacity/shoestore/store/ShoeViewModel.kt
@@ -3,9 +3,8 @@ package com.udacity.shoestore.store
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import timber.log.Timber
 
-class ShoeViewModel: ViewModel() {
+class ShoeViewModel : ViewModel() {
 
     private val _eventViewDetails = MutableLiveData<Boolean>()
     val eventViewDetails: LiveData<Boolean>
